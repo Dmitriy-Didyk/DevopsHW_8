5b4686beb40489641103816011f130476ef645f8
Dmitriy-Didyk <didyk.dmitriy@apriorit.com>
2024-11-12 15:25:55 +0200

    Delete provider.tf

--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "azurerm" {
-  features {}
-
-  # Укажите ID подписки, клиента, секрет и tenant для аутентификации
-  subscription_id = "14837371-039f-4e1d-8ae8-af5e733ade86"
-  client_id       = "d77e620a-2d05-4e5d-aaeb-ae1c5302a70a"
-  client_secret   = "oY88Q~EwjL1~2wgKUTt0QumeATIKK3WaiOY_Fal9"
-  tenant_id       = "fed13a2f-5620-4dae-85d0-9f757ae5e262"
-}
